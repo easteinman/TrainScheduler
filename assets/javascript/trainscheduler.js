676dcc5748cf085a05197aaf519e18ffe33ad995
Eric <ericsteinman@me.com>
2018-01-21 18:23:22 -0600

    Boom, everything is up and runnning. Used the timesheet example a lot. App works perfectly. Will work on making it look prettier later

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -19,14 +19,15 @@ $("#add-train-btn").on("click", function(event) {
   // Grabs user input
   var trainName = $("#train-name-input").val().trim();
   var trainDest = $("#destination-input").val().trim();
-  var trainTime = moment($("#train-time-input").val().trim(), "HH:mm").format("X");
+  var trainStart = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
   var trainFreq = $("#frequency-input").val().trim();
+  
 
   // Creates local "temporary" object for holding train data
   var newTrain = {
     name: trainName,
     destination: trainDest,
-    time: trainTime,
+    start: trainStart,
     frequency: trainFreq
   };
 
@@ -36,35 +37,44 @@ $("#add-train-btn").on("click", function(event) {
   // Logs everything to console
   console.log(newTrain.name);
   console.log(newTrain.destination);
-  console.log(newTrain.time);
+  console.log(newTrain.start);
   console.log(newTrain.frequency);
 
   // Alert
-  alert("Train successfully added");
+  alert("Choo choo! The train has been added successfully.");
 
   // Clears all of the text-boxes
   $("#train-name-input").val("");
   $("#destination-input").val("");
-  $("#train-time-input").val("");
+  $("#first-train-input").val("");
   $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+// Create a Firebase event for adding the new train to the database and add a row in the html when a user adds a new train
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
 
   // Store everything into a variable.
   var trainName = childSnapshot.val().name;
-  var trainDest = childSnapshot.val().role;
-  var trainTime = childSnapshot.val().start;
-  var trainFreq = childSnapshot.val().rate;
+  var trainDest = childSnapshot.val().destination;
+  var trainStart = childSnapshot.val().start;
+  var frequency = childSnapshot.val().frequency;
 
-  // Train Info
-  console.log(trainName);
-  console.log(trainDest);
-  console.log(trainTime);
-  console.log(trainFreq);
+  // Calculating the difference between the first train time and the current time
+  var difference = moment().diff(moment.unix(trainStart), "minutes");
 
-  // Prettify the train time
-  var trainTimePretty = moment.unix(trainTime).format("HH:mm");
+  // Calculating the number of times the train has arrived since it started until now
+  var timeLeft = moment().diff(moment.unix(trainStart), 'minutes') % frequency;
+
+  // Calculating how many minutes are left until the next train
+  var mins = moment(frequency - timeLeft, "mm").format('mm');
+
+  // Add the minutes left to the current time to show the time the next train will arrive
+  var nextTrain = moment().add(mins, "m").format("hh:mm A");
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
+  frequency + "</td><td>" + nextTrain + "</td><td>" + mins + "</td></tr>");
+
+});
